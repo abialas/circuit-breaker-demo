4588d59ce17c344d0ce0e7da7911ee79a424503a
abialas <abialas@universal-soft.com>
2020-10-29 23:22:32 +0100

    Rename so config does not interfere with resilience4j circuit breaker config

--- creditscore-api/src/main/java/com/us/mortgage/creditscoreapi/CircuitBreakerConfiguration.java
+++ creditscore-api/src/main/java/com/us/mortgage/creditscoreapi/CircuitBreakerConfiguration.java
@@ -1,5 +1,6 @@
 package com.us.mortgage.creditscoreapi;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
@@ -13,18 +14,20 @@ import java.time.Duration;
  * Created by adam.
  */
 @Configuration
-class CircuitBreakerConfig {
+class CircuitBreakerConfiguration {
 
     @Bean
     public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
         return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
-                .circuitBreakerConfig(io.github.resilience4j.circuitbreaker.CircuitBreakerConfig
+                .circuitBreakerConfig(CircuitBreakerConfig
                         .custom()
                         .minimumNumberOfCalls(10)
                         .slidingWindowSize(10)
-                        .waitDurationInOpenState(Duration.ofMinutes(1))
+                        .waitDurationInOpenState(Duration.ofSeconds(10))
                         .build())
-                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(1)).build())
+                .timeLimiterConfig(TimeLimiterConfig
+                        .custom()
+                        .timeoutDuration(Duration.ofSeconds(5)).build())
                 .build());
     }
 
